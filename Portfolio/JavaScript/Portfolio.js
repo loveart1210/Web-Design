6107fad3e1ff40e913332c7b069b73872b86a835
loveart1210 <bachnguyengia2@gmail.com>
2024-10-24 02:16:52 +0700

    Delete Portfolio directory

--- Portfolio/JavaScript/Portfolio.js
+++
@@ -1,9 +0,0 @@
-function openMenu() {
-    document.getElementById("side-bar").classList.add("open");
-    document.getElementById("menu-overlay").classList.add("active");
-}
-
-function closeMenu() {
-    document.getElementById("side-bar").classList.remove("open");
-    document.getElementById("menu-overlay").classList.remove("active");
-}
